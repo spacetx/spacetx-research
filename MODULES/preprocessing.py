dea84b38838e958045935e5934954b4bdd8ec1b5
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-08-15 04:54:45 +0000

    change preprocessing

--- MODULES/preprocessing.py
+++ MODULES/preprocessing.py
@@ -1,9 +1,15 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from IPython.display import HTML
+
 import skimage.exposure
 import numpy as np
 from .namedtuple import ImageBbox
 import PIL.Image
 PIL.Image.MAX_IMAGE_PIXELS = None
 from typing import Optional
+
+
 
 
 def pil_to_numpy(pilfile, mode: str = 'L', reduction_factor: int = 1):
@@ -16,70 +22,42 @@ def pil_to_numpy(pilfile, mode: str = 'L', reduction_factor: int = 1):
     assert (mode == 'L' or mode == 'RGB' or mode == 'I' or mode == 'F')
 
     pilobject = PIL.Image.open(pilfile).convert(mode)
-
-    # Use PIL.Image.LANCZOS pto revent antialiasing when downsampling
-    if reduction_factor != 1:
-        w_raw, h_raw = pilobject.size
-        w_new = int(w_raw / reduction_factor)
-        h_new = int(h_raw / reduction_factor)
-        pilobject = pilobject.resize((w_new, h_new), resample=PIL.Image.BILINEAR)
-
-    return skimage.exposure.rescale_intensity(image=np.array(pilobject), 
-                                              in_range='image', 
-                                              out_range='dtype')
-
-
-def rescale_intensity(img_reference: np.ndarray, 
-                      histo_mask: Optional[np.ndarray] = None, 
-                      q: tuple = (0.01, 99.99), 
-                      gamma: float = 1.0):
-    """ Rescale intensity inside the histo_mask so that the intensities occupy the full range
-        For gamma greater than 1, the histogram will shift towards left and
-        the output image will be darker than the input image.
-        For gamma less than 1, the histogram will shift towards right and
-        the output image will be brighter than the input image."""
-
-    # Here I am just computing ql,qr
-    if histo_mask is None:
-        ql, qr = np.percentile(img_reference.reshape(-1), q=q)
-    else:
-        ql, qr = np.percentile(img_reference[histo_mask].reshape(-1), q=q)
+    array = skimage.exposure.rescale_intensity(image=np.array(pilobject), 
+                                               in_range='image', 
+                                               out_range='dtype')
     
-    # Define the pre-processing which uses: ql, qr
-    def f(x):
-        x1 = skimage.exposure.rescale_intensity(x,
-                                                in_range=(ql, qr),
-                                                out_range='dtype')
-        if gamma == 1:
-            return x1
-        else:
-            x2 = skimage.exposure.adjust_gamma(x1, gamma=gamma, gain=1)
-            return x2
-
-    img_out = f(img_reference)
-    return img_out, f
+    return array
 
 
-def compute_sqrt_mse(predictions: np.ndarray, 
+def gaussian_smoothing(img: np.ndarray, sigma_gaussian_filter: np.ndarray = np.arange(0,10)):
+    
+    # sigma=0 has no smoothing, i.e. original image
+    smoothed = np.zeros([sigma_gaussian_filter.shape[0]]+list(img.shape))
+    multichannel = (len(img.shape)==3)
+    for n,s in enumerate(sigma_gaussian_filter):
+        smoothed[n] = skimage.filters.gaussian(img, sigma=s, multichannel=multichannel)
+    return smoothed
+
+
+def compute_sqrt_mse(smoothed: np.ndarray, 
                      roi_mask: Optional[np.ndarray] = None):
     if roi_mask is None:
-        w,h = predictions.shape[-2:]
-        roi_mask = np.ones((w,h), dtype=predictions.dtype)
+        w,h = smoothed.shape[-2:]
+        roi_mask = np.ones((w,h), dtype=smoothed.dtype)
         
     assert len(roi_mask.shape) == 2
-    if len(predictions.shape) == 3:
+    if len(smoothed.shape) == 3:
         # no channel
-        diff = (predictions - predictions[0]) * roi_mask
+        diff = (smoothed - smoothed[0]) * roi_mask
         mse = np.sum(diff ** 2, axis=(-1, -2)) / np.sum(roi_mask)
-    elif len(predictions.shape) == 4:
+    elif len(smoothed.shape) == 4:
         # yes channel
-        c = predictions.shape[-1]
-        diff = (predictions - predictions[0]) * roi_mask[..., None]
+        c = smoothed.shape[-1]
+        diff = (smoothed - smoothed[0]) * roi_mask[..., None]
         mse = np.sum(diff ** 2, axis=(-1, -2, -3)) / (c * np.sum(roi_mask))
     else:
         raise Exception("Yopu should never be here")
-    sigma = np.sqrt(mse)
-    return sigma
+    return np.sqrt(mse)
 
 
 def getLargestCC(mask):
@@ -101,6 +79,172 @@ def find_bbox(mask):
                      min_col=min_col,
                      max_row=max_row,
                      max_col=max_col)
+
+
+#-----------------------------------------
+#-- VISUALIZATION HELPER FUNCTIONS -------
+#-----------------------------------------
+
+def show_random_examples(img: np.ndarray,
+                         nexamples: int = 9, 
+                         ncols: int = 3, 
+                         size: int = 200,
+                         figsize: tuple = (12,12)):
+    
+    nrows=int(np.ceil(nexamples/ncols))
+    iw_array = np.random.randint(low=0, high=img.shape[-2]-size, size=nexamples, dtype=int)
+    ih_array = np.random.randint(low=0, high=img.shape[-1]-size, size=nexamples, dtype=int)
+
+    if nrows == 1:
+        figure, axes = plt.subplots(ncols=ncols, figsize=figsize)
+        for n in range(nexamples):
+            axes[n].imshow(img[iw_array[n]:iw_array[n]+size,
+                               ih_array[n]:ih_array[n]+size], cmap='gray')
+    else:
+        figure, axes = plt.subplots(ncols=ncols, nrows=nrows, figsize=figsize)
+        for n in range(nexamples):
+            r = int(n / ncols)
+            c = np.mod(n,ncols)
+            axes[r,c].imshow(img[iw_array[n]:iw_array[n]+size,
+                                 ih_array[n]:ih_array[n]+size], cmap='gray')
+        
+def plot_img_and_histo(img: np.ndarray, 
+                       img_gray: Optional[np.ndarray] = None,
+                       histo_mask: Optional[np.ndarray] = None, 
+                       figsize=(24, 12)):
+    if histo_mask is None:
+        histo_mask = np.ones(img.shape[:2], dtype=bool)
+    
+    if img_gray is None:
+        figure, axes = plt.subplots(ncols=2, figsize=figsize)
+        a0 = axes[0]
+        a1 = axes[1]
+      
+    else:
+        figure, axes = plt.subplots(ncols=2, nrows=2, figsize=figsize)
+        a0 = axes[0,0]
+        a1 = axes[0,1]
+        a2 = axes[1,0]
+        a3 = axes[1,1]
+        _ = a2.imshow(img_gray, cmap='gray')
+        _ = a3.hist(img_gray[histo_mask].reshape(-1), density=True, bins=100)
+        _ = a2.set_title("image_GRAY")
+        _ = a3.set_title("histogram image_GRAY")
+        
+    _ = a0.imshow(img)
+    _ = a1.hist(img[histo_mask].reshape(-1), density=True, bins=100)
+    _ = a0.set_title("image")
+    _ = a1.set_title("histogram image")
+    
+def plot_img_and_zoom(window: tuple,
+                      img: Optional[np.ndarray] = None,
+                      img_gray: Optional[np.ndarray] = None, 
+                      figsize: tuple = (24, 12)):
+    
+    w0,h0,w1,h1 = window
+    
+    if img is None and img_gray is None:
+        raise Exception("Nothing to plot. Either img or img_gray should be not zero")
+    elif (img is not None) and (img_gray is not None): 
+        figure, axes = plt.subplots(ncols=2, nrows=2, figsize=figsize)
+        a0 = axes[0,0]
+        a2 = axes[0,1]
+        a1 = axes[1,0]
+        a3 = axes[1,1]
+        _ = a0.imshow(img)
+        _ = a1.imshow(img[w0:w1,h0:h1])
+        _ = a0.set_title("image")
+        _ = a1.set_title("image, zoom")
+        _ = a2.imshow(img_gray, cmap="gray")
+        _ = a3.imshow(img_gray[w0:w1,h0:h1], cmap="gray")
+        _ = a2.set_title("image_GRAY")
+        _ = a3.set_title("image_GRAY, zoom")
+    elif (img is not None):
+        figure, axes = plt.subplots(ncols=2, figsize=figsize)
+        a0 = axes[0]
+        a1 = axes[1]
+        _ = a0.imshow(img)
+        _ = a1.imshow(img[w0:w1,h0:h1])
+        _ = a0.set_title("image")
+        _ = a1.set_title("image, zoom")
+    else:
+        figure, axes = plt.subplots(ncols=2, figsize=figsize)
+        a0 = axes[0]
+        a1 = axes[1]
+        _ = a0.imshow(img_gray, cmap='gray')
+        _ = a1.imshow(img_gray[w0:w1,h0:h1], cmap='gray')
+        _ = a0.set_title("image_GRAY")
+        _ = a1.set_title("image_GRAY, zoom")
+
+        
+def plot_img_and_nuclei(img_gray, NUCLEI_mask, window, figsize=(24, 12)):
+
+    w0,h0,w1,h1 = window 
+    figure, axes = plt.subplots(ncols=3, nrows=2, figsize=figsize)
+    gray_img = axes[0,0]
+    instances = axes[0,1]
+    overlay = axes[0,2]
+
+    gray_img_zoom = axes[1,0]
+    instances_zoom = axes[1,1]
+    overlay_zoom = axes[1,2]
+
+    gray_img.imshow(img_gray, cmap='gray')
+    instances.imshow(NUCLEI_mask, cmap='gray')
+    overlay.imshow(img_gray)
+    overlay.imshow(NUCLEI_mask, alpha=0.5, cmap='gray')
+
+    gray_img_zoom.imshow(img_gray[w0:w1,h0:h1], cmap='gray')
+    instances_zoom.imshow(NUCLEI_mask[w0:w1,h0:h1], cmap='gray')
+    overlay_zoom.imshow(img_gray[w0:w1,h0:h1])
+    overlay_zoom.imshow(NUCLEI_mask[w0:w1,h0:h1], alpha=0.5)#, cmap='gray')
+
+    _ = gray_img.set_title("RAW GRAYSCALE image")
+    _ = gray_img_zoom.set_title("RAW GRAYSCALE image, zoom")
+    _ = instances.set_title("NUCLEI only")
+    _ = instances_zoom.set_title("NUCLEI only, zoom")
+    _ = overlay.set_title("overlay image and nuclei")
+    _ = overlay_zoom.set_title("overlay image and nuclei, zoom")
+    
+    
+def show_video(frames: np.ndarray,
+               ref_image: np.ndarray,
+               mse: np.ndarray,
+               figsize: tuple = (8, 4),
+               interval: int = 50):
+    """
+    :param video: an ndarray with shape (n_frames, height, width, 3)
+    """
+    fig, axs = plt.subplots(ncols=2, figsize=figsize)
+    ax_image = axs[0]
+    ax_video = axs[1]
+    
+    # image
+    ax_image.axis('off')
+    ax_image.imshow(ref_image)
+    ax_image.set_title("ref image")
+    
+    ax_video.axis('off')
+    ax_video.imshow(frames[0, ...])
+    ax_video.set_title("title")
+        
+    plt.tight_layout()
+    plt.close()
+    
+    def init():
+        ax_video.imshow(frames[0, ...])
+        ax_video.set_title("title")
+        
+    def animate(i):
+        title = 'corrupted prediction, frame={0:3d}, sigma={1:.3f}'.format(i,mse[i])
+        ax_video.imshow(frames[i, ...])
+        ax_video.set_title(title)
+        
+    anim = animation.FuncAnimation(
+        fig, animate, init_func=init, frames=frames.shape[0],
+        interval=interval)
+    
+    return HTML(anim.to_html5_video())
 
 
 #####def normalize_tensor(image, scale_each_image=False, scale_each_channel=False, in_place=False):
