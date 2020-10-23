1481e7c00bebd61c13ce01e949d02db92aa2338d
dalessioluca <ldalessi@broadinstitute.org>
2020-10-22 21:33:00 -0400

    fixed default value of lenght scale in similarity

--- src/MODULES/utilities_ml.py
+++ src/MODULES/utilities_ml.py
@@ -94,7 +94,7 @@ class SimilarityKernel(torch.nn.Module):
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
         if length_scales is None:
-            LENGTH_2 = 10.0
+            LENGTH_2 = 0.1
             length_scales = torch.linspace(LENGTH_2/self.n_kernels, LENGTH_2,
                                            steps=self.n_kernels,
                                            device=self.device,
