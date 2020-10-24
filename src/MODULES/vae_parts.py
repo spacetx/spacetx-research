0f636de5f6206f895aa7d51c19507faa17a90b6a
dalessioluca <ldalessi@broadinstitute.org>
2020-10-23 22:27:09 -0400

    multiplied by c_few after w_to_pi

--- src/MODULES/vae_parts.py
+++ src/MODULES/vae_parts.py
@@ -185,8 +185,8 @@ class Inference_and_Generation(torch.nn.Module):
         # ------------------------
         # TODO: try both q_few and c_few
         mixing_tmp, mixing_non_interacting_tmp = from_w_to_pi(weight=big_weight, dim=-5)
-        mixing = q_few[..., None, None, None] * mixing_tmp
-        mixing_non_interacting = mixing_non_interacting_tmp * q_few[..., None, None, None]
+        mixing = c_few[..., None, None, None] * mixing_tmp
+        mixing_non_interacting = mixing_non_interacting_tmp * c_few[..., None, None, None]
 
         # 8. Return the inferred quantities
         similarity_l, similarity_w = self.similarity_kernel_dpp.get_l_w()
